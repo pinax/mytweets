6a37445441e50d51fc4a0343f78fa98e396b462d
Patrick Altman <paltman@gmail.com>
2016-04-21 08:45:02 -0500

    Add pinax-types filtering

--- mytweets/views.py
+++ mytweets/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView
 
+from pinax.types.periods import get_period
+
 from .models import Tweet
 
 
@@ -8,3 +10,12 @@ class TweetListView(ListView):
     template_name = "homepage.html"
     paginate_by = 100
     ordering = "-pk"
+
+    def get_queryset(self):
+        queryset = super(TweetListView, self).get_queryset()
+        period = self.request.GET.get("period")
+        if period:
+            queryset = queryset.filter(
+                timestamp__range=get_period(period).get_start_end()
+            )
+        return queryset
